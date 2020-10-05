e12a805a857e544dee86b126fa88048bda9003b9
Lucas Fell <lucasafell@gmail.com>
2020-10-05 18:11:07 -0300

    preferencia5 - teachers and subjects.

--- Entrega 2/swish.pl
+++ Entrega 2/swish.pl
@@ -66,3 +66,239 @@ semestre5(etica_e_legislacao_em_computacao).
 semestre5(medicao_e_analise).
 semestre5(resolucao_de_problemas_v).
 semestre5(redes_de_computadores_es).
+
+preferencia5(professor1, administracao_e_empreendedorismo).
+preferencia5(professor1, engenharia_economica).
+preferencia5(professor1, etica_e_legislacao_em_computacao).
+preferencia5(professor1, inovacao_e_criatividade).
+preferencia5(professor1, analise_e_projeto_de_software).
+preferencia5(professor1, medicao_e_analise).
+preferencia5(professor1, resolucao_de_problemas_i).
+preferencia5(professor1, resolucao_de_problemas_iii).
+preferencia5(professor1, resolucao_de_problemas_v).
+preferencia5(professor1, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor1, linguagens_formais_es).
+preferencia5(professor1, redes_de_computadores_es).
+
+preferencia5(professor2, engenharia_economica).
+preferencia5(professor2, etica_e_legislacao_em_computacao).
+preferencia5(professor2, analise_e_projeto_de_software).
+preferencia5(professor2, medicao_e_analise).
+preferencia5(professor2, resolucao_de_problemas_iii).
+preferencia5(professor2, resolucao_de_problemas_v).
+preferencia5(professor2, linguagens_formais_es).
+preferencia5(professor2, redes_de_computadores_es).
+
+preferencia5(professor3, administracao_e_empreendedorismo).
+preferencia5(professor3, engenharia_economica).
+preferencia5(professor3, etica_e_legislacao_em_computacao).
+preferencia5(professor3, inovacao_e_criatividade).
+preferencia5(professor3, analise_e_projeto_de_software).
+preferencia5(professor3, medicao_e_analise).
+preferencia5(professor3, resolucao_de_problemas_i).
+preferencia5(professor3, resolucao_de_problemas_iii).
+preferencia5(professor3, resolucao_de_problemas_v).
+preferencia5(professor3, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor3, estruturas_de_dados_es).
+preferencia5(professor3, redes_de_computadores_es).
+
+preferencia5(professor4, administracao_e_empreendedorismo).
+preferencia5(professor4, engenharia_economica).
+preferencia5(professor4, etica_e_legislacao_em_computacao).
+preferencia5(professor4, inovacao_e_criatividade).
+preferencia5(professor4, analise_e_projeto_de_software).
+preferencia5(professor4, medicao_e_analise).
+preferencia5(professor4, resolucao_de_problemas_i).
+preferencia5(professor4, resolucao_de_problemas_v).
+preferencia5(professor4, linguagens_formais_es).
+preferencia5(professor4, matematica_discreta_es).
+
+preferencia5(professor5, administracao_e_empreendedorismo).
+preferencia5(professor5, engenharia_economica).
+preferencia5(professor5, medicao_e_analise).
+preferencia5(professor5, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor5, linguagens_formais_es).
+preferencia5(professor5, redes_de_computadores_es).
+
+preferencia5(professor6, administracao_e_empreendedorismo).
+preferencia5(professor6, engenharia_economica).
+preferencia5(professor6, resolucao_de_problemas_iii).
+preferencia5(professor6, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor6, linguagens_formais_es).
+preferencia5(professor6, redes_de_computadores_es).
+preferencia5(professor6, logica_matematica_es).
+preferencia5(professor6, matematica_discreta_es).
+
+preferencia5(professor7, administracao_e_empreendedorismo).
+preferencia5(professor7, engenharia_economica).
+preferencia5(professor7, etica_e_legislacao_em_computacao).
+preferencia5(professor7, inovacao_e_criatividade).
+preferencia5(professor7, medicao_e_analise).
+preferencia5(professor7, resolucao_de_problemas_i).
+preferencia5(professor7, resolucao_de_problemas_iii).
+preferencia5(professor7, resolucao_de_problemas_v).
+preferencia5(professor7, linguagens_formais_es).
+preferencia5(professor7, redes_de_computadores_es).
+preferencia5(professor7, logica_matematica_es).
+preferencia5(professor7, matematica_discreta_es).
+
+preferencia5(professor8, administracao_e_empreendedorismo).
+preferencia5(professor8, engenharia_economica).
+preferencia5(professor8, etica_e_legislacao_em_computacao).
+preferencia5(professor8, inovacao_e_criatividade).
+preferencia5(professor8, analise_e_projeto_de_software).
+preferencia5(professor8, medicao_e_analise).
+preferencia5(professor8, resolucao_de_problemas_i).
+preferencia5(professor8, resolucao_de_problemas_iii).
+preferencia5(professor8, resolucao_de_problemas_v).
+
+preferencia5(professor9, engenharia_economica).
+preferencia5(professor9, analise_e_projeto_de_software).
+preferencia5(professor9, medicao_e_analise).
+preferencia5(professor9, resolucao_de_problemas_iii).
+preferencia5(professor9, resolucao_de_problemas_v).
+preferencia5(professor9, algoritmos_e_programacao_es).
+preferencia5(professor9, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor9, estruturas_de_dados_es).
+preferencia5(professor9, linguagens_formais_es).
+preferencia5(professor9, redes_de_computadores_es).
+preferencia5(professor9, logica_matematica_es).
+preferencia5(professor9, matematica_discreta_es).
+
+preferencia5(professor10, administracao_e_empreendedorismo).
+preferencia5(professor10, engenharia_economica).
+preferencia5(professor10, etica_e_legislacao_em_computacao).
+preferencia5(professor10, analise_e_projeto_de_software).
+preferencia5(professor10, medicao_e_analise).
+preferencia5(professor10, resolucao_de_problemas_v).
+preferencia5(professor10, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor10, linguagens_formais_es).
+preferencia5(professor10, logica_matematica_es).
+preferencia5(professor10, matematica_discreta_es).
+
+preferencia5(professor11, engenharia_economica).
+preferencia5(professor11, etica_e_legislacao_em_computacao).
+preferencia5(professor11, inovacao_e_criatividade).
+preferencia5(professor11, resolucao_de_problemas_iii).
+preferencia5(professor11, algoritmos_e_programacao_es).
+preferencia5(professor11, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor11, estruturas_de_dados_es).
+preferencia5(professor11, linguagens_formais_es).
+preferencia5(professor11, redes_de_computadores_es).
+preferencia5(professor11, logica_matematica_es).
+preferencia5(professor11, matematica_discreta_es).
+
+preferencia5(professor12, administracao_e_empreendedorismo).
+preferencia5(professor12, engenharia_economica).
+preferencia5(professor12, etica_e_legislacao_em_computacao).
+preferencia5(professor12, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor12, redes_de_computadores_es).
+preferencia5(professor12, logica_matematica_es).
+preferencia5(professor12, matematica_discreta_es).
+
+preferencia5(professor13, administracao_e_empreendedorismo).
+preferencia5(professor13, engenharia_economica).
+preferencia5(professor13, etica_e_legislacao_em_computacao).
+preferencia5(professor13, inovacao_e_criatividade).
+preferencia5(professor13, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor13, linguagens_formais_es).
+preferencia5(professor13, redes_de_computadores_es).
+preferencia5(professor13, logica_matematica_es).
+preferencia5(professor13, matematica_discreta_es).
+
+preferencia5(professor14, administracao_e_empreendedorismo).
+preferencia5(professor14, engenharia_economica).
+preferencia5(professor14, inovacao_e_criatividade).
+preferencia5(professor14, medicao_e_analise).
+preferencia5(professor14, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor14, linguagens_formais_es).
+preferencia5(professor14, redes_de_computadores_es).
+preferencia5(professor14, logica_matematica_es).
+preferencia5(professor14, matematica_discreta_es).
+
+preferencia5(professor15, administracao_e_empreendedorismo).
+preferencia5(professor15, engenharia_economica).
+preferencia5(professor15, etica_e_legislacao_em_computacao).
+preferencia5(professor15, inovacao_e_criatividade).
+preferencia5(professor15, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor15, linguagens_formais_es).
+preferencia5(professor15, redes_de_computadores_es).
+preferencia5(professor15, logica_matematica_es).
+preferencia5(professor15, matematica_discreta_es).
+
+preferencia5(professor16, administracao_e_empreendedorismo).
+preferencia5(professor16, engenharia_economica).
+preferencia5(professor16, etica_e_legislacao_em_computacao).
+preferencia5(professor16, inovacao_e_criatividade).
+preferencia5(professor16, analise_e_projeto_de_software).
+preferencia5(professor16, medicao_e_analise).
+preferencia5(professor16, resolucao_de_problemas_i).
+preferencia5(professor16, resolucao_de_problemas_iii).
+preferencia5(professor16, resolucao_de_problemas_v).
+preferencia5(professor16, algoritmos_e_programacao_es).
+preferencia5(professor16, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor16, estruturas_de_dados_es).
+preferencia5(professor16, redes_de_computadores_es).
+
+preferencia5(professor17, engenharia_economica).
+preferencia5(professor17, etica_e_legislacao_em_computacao).
+preferencia5(professor17, inovacao_e_criatividade).
+preferencia5(professor17, algoritmos_e_programacao_es).
+preferencia5(professor17, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor17, estruturas_de_dados_es).
+preferencia5(professor17, redes_de_computadores_es).
+preferencia5(professor17, matematica_discreta_es).
+
+preferencia5(professor18, administracao_e_empreendedorismo).
+preferencia5(professor18, engenharia_economica).
+preferencia5(professor18, etica_e_legislacao_em_computacao).
+preferencia5(professor18, inovacao_e_criatividade).
+preferencia5(professor18, analise_e_projeto_de_software).
+preferencia5(professor18, medicao_e_analise).
+preferencia5(professor18, resolucao_de_problemas_i).
+preferencia5(professor18, resolucao_de_problemas_iii).
+preferencia5(professor18, resolucao_de_problemas_v).
+preferencia5(professor18, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor18, linguagens_formais_es).
+preferencia5(professor18, redes_de_computadores_es).
+preferencia5(professor18, logica_matematica_es).
+preferencia5(professor18, matematica_discreta_es).
+
+preferencia5(professor19, administracao_e_empreendedorismo).
+preferencia5(professor19, engenharia_economica).
+preferencia5(professor19, etica_e_legislacao_em_computacao).
+preferencia5(professor19, inovacao_e_criatividade).
+preferencia5(professor19, analise_e_projeto_de_software).
+preferencia5(professor19, medicao_e_analise).
+preferencia5(professor19, resolucao_de_problemas_i).
+preferencia5(professor19, resolucao_de_problemas_iii).
+preferencia5(professor19, resolucao_de_problemas_v).
+preferencia5(professor19, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor19, linguagens_formais_es).
+preferencia5(professor19, redes_de_computadores_es).
+preferencia5(professor19, matematica_discreta_es).
+
+preferencia5(professor20, administracao_e_empreendedorismo).
+preferencia5(professor20, engenharia_economica).
+preferencia5(professor20, etica_e_legislacao_em_computacao).
+preferencia5(professor20, inovacao_e_criatividade).
+preferencia5(professor20, medicao_e_analise).
+preferencia5(professor20, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor20, linguagens_formais_es).
+preferencia5(professor20, redes_de_computadores_es).
+preferencia5(professor20, logica_matematica_es).
+
+preferencia5(professor21, administracao_e_empreendedorismo).
+preferencia5(professor21, engenharia_economica).
+preferencia5(professor21, etica_e_legislacao_em_computacao).
+preferencia5(professor21, inovacao_e_criatividade).
+preferencia5(professor21, linguagens_formais_es).
+preferencia5(professor21, logica_matematica_es).
+preferencia5(professor21, matematica_discreta_es).
+
+preferencia5(professor22, engenharia_economica).
+preferencia5(professor22, resolucao_de_problemas_iii).
+preferencia5(professor22, arquitetura_e_organizacao_de_computadores_es).
+preferencia5(professor22, linguagens_formais_es).
+preferencia5(professor22, logica_matematica_es).
+preferencia5(professor22, matematica_discreta_es).
