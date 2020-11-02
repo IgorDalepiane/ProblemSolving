ceed0708a149bb2507385491fd45c65fef15f974
Matheus Kildere Conceicao de Vascocelos <matheuskildery94@gmail.com>
2020-11-02 18:41:57 -0300

    feat: Client data model and teste save data on txt file

--- Entrega 3/Locar.hs
+++ Entrega 3/Locar.hs
@@ -35,12 +35,38 @@ menuClient = do
     if (read option) == 0 then menu else do selectedOptionClient (read option)
 
 selectedOptionClient :: Int -> IO()
-selectedOptionClient opcao | opcao == 1 = do {putStrLn "Opcao em Desenvolvimento"; menu} 
+selectedOptionClient opcao | opcao == 1 = do {addNewClient (Client {clientId = 2, name = "kildere", idCNH = "54648", programPoints = 5}); menu} 
                      | opcao == 2 = do {putStrLn "Opcao em Desenvolvimento"; menu}
                      | opcao == 3 = do {putStrLn "Opcao em Desenvolvimento"; menu}
                      | opcao == 4 = do {putStrLn "Opcao em Desenvolvimento"; menu}
                      | otherwise =  do {putStrLn "Opcao em Desenvolvimento"; menu}
 
+
+------------------------DATA PERSISTENCE-----------------------
+
+addNewClient :: Client -> IO()
+addNewClient client = do
+    let conteudo = conteudoAdicionar client
+    appendFile "clints.txt" (conteudo)
+    where
+        conteudoAdicionar :: Client -> String
+        conteudoAdicionar client
+            | 2 == (-1) = ""
+            | otherwise = ((clientToString (client)) ++ "\n")
+
+------------------------MODELS-----------------------
+
+data Client = Client {
+    clientId :: Integer,
+    name :: String,
+    idCNH :: String,
+    programPoints :: Integer
+} deriving (Show)
+
+clientToString :: Client -> String
+clientToString (Client {clientId = clientId, name = name, idCNH = idCNH, programPoints = lo}) = show clientId ++ " - " ++ name ++ " - " ++ idCNH ++ " - " ++ show(lo)
+
+------------------------LOGOS------------------------
 logo :: IO ()
 logo = do
     putStrLn "    __    ____  _________    ____ " 
