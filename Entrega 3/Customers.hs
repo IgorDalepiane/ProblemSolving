afde1fa598341404ded6933933c348af50f29190
Brandow Buenos Aires Madeira <brandowbuenosaires@gmail.com>
2020-11-10 17:41:28 -0300

    feat: add list customers
    Feat. Matheus Kildere

--- Entrega 3/Customers.hs
+++ Entrega 3/Customers.hs
@@ -3,11 +3,9 @@
 
 module Customers where
 
-import Control.Applicative
 import Data.Aeson
 import GHC.Generics
 import qualified Data.ByteString.Lazy as B
-import Data.List
 
 data Customer = Customer { 
     customerId :: Int,
@@ -26,7 +24,7 @@ menuCustomers = do
   putStrLn "1 - Cadastrar novo cliente"
   putStrLn "2 - Alterar dados de cliente"
   putStrLn "3 - Excluir clientes"
-  putStrLn "4 - Consultar cliente"
+  putStrLn "4 - Listar clientes"
   putStrLn "0 - voltar"
   putStrLn "Opcao: "
   option <- getLine
@@ -37,8 +35,10 @@ selectedOptionCustomer opcao
   | opcao == 1 = do addCustomer; menuCustomers
   | opcao == 2 = do readFromJSON; menuCustomers
   | opcao == 3 = do readFromJSON; menuCustomers
-  | opcao == 4 = do readFromJSON; menuCustomers
+  | opcao == 4 = do lista <- readFromJSON; printCustomers lista; menuCustomers
   | otherwise = do readFromJSON; menuCustomers
+
+-- Add Customer
 
 addCustomer :: IO ()
 addCustomer = do
@@ -64,6 +64,17 @@ addToList :: [Customer] -> Customer -> [Customer]
 addToList [] x = [x]
 addToList x ve = x ++ [ve]
 
+-- List customers
+
+printCustomers :: [Customer] -> IO ()
+printCustomers customers = putStrLn ("\n\nId - CNH - Nome do cliente - Pontos de fidelidade\n\n" ++ (listCustomer customers) ++ "\n")
+
+listCustomer :: [Customer] -> String
+listCustomer [] = ""
+listCustomer (x : xs) = toStringCustomer x ++ ['\n'] ++ listCustomer xs
+
+-- JSON actions
+
 writeToJSON :: [Customer] -> IO ()
 writeToJSON list = do
   B.writeFile "db/customers.json" (encode list)
@@ -77,13 +88,6 @@ readFromJSON = do
   case customers of
     Nothing -> return []
     Just customers -> return customers
-
-printCustomers :: [Customer] -> IO ()
-printCustomers customers = putStrLn ("\n\n\n" ++ (listCustomer customers) ++ "\n\n")
-
-listCustomer :: [Customer] -> String
-listCustomer [] = ""
-listCustomer (x : xs) = toStringCustomer x ++ ['\n'] ++ listCustomer xs
 
 toStringCustomer :: Customer -> String
 toStringCustomer (Customer {customerId = i, idCNH = cnh, name = n, programPoints = pp}) = show i ++ " - " ++ cnh ++ " - " ++ n ++ " - " ++ show pp ++ "pts"
