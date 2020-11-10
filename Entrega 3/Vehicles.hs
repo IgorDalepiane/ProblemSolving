ddc61d26a34ba16a4feba6ce61cfd36c73002de1
Igor Dalepiane <igordalepiane@gmail.com>
2020-11-10 16:24:40 -0300

    remove vehicle, new fields for data vehicle, genVehicleId, feat Lucas

--- Entrega 3/Vehicles.hs
+++ Entrega 3/Vehicles.hs
@@ -3,14 +3,18 @@
 module Vehicles where
 
 import Data.Aeson
-import GHC.Generics
-import Control.Applicative
-import Data.List
+    ( decode,
+      encode,
+      defaultOptions,
+      genericToEncoding,
+      FromJSON,
+      ToJSON(toEncoding) )
+import GHC.Generics ( Generic )
 import qualified Data.ByteString.Lazy as B
 
 data Vehicle = Vehicle {
-  placa, category :: String,
-  indice, kms :: Int
+  vehicleId, kms, year :: Int,
+  plate, category, model, brand, color :: String
 } deriving (Generic, Show)
 
 instance ToJSON Vehicle where
@@ -33,30 +37,40 @@ selectedOption :: Int -> IO()
 selectedOption option | option == 1 = do {lista <- readFromJSON; printVehicles lista}
                       | option == 2 = do {optionAddVehicle}
                       | option == 3 = do {menuVehicles}
-                      | option == 4 = do {menuVehicles}
+                      | option == 4 = do {optionRemoveVehicle}
 
 
--- rmVehicle :: [Vehicle] -> String -> Maybe Vehicle
--- rmVehicle ve idx = do
---   find idx ve
+removeItem :: Int -> [Vehicle] -> [Vehicle]
+removeItem _ []                       = []
+removeItem x (y:ys) | x == (vehicleId) y = removeItem x ys
+                    | otherwise = y   : removeItem x ys
 
--- optionRemoveVehicle :: IO()
--- optionRemoveVehicle = do 
---   putStrLn "\n\n\n### Remoção de veículo ###\n\n\n"
---   putStrLn "Índice do veículo: "
---   _indice <- getLine
---   lista <- readFromJSON
---   let rmedVehicle = rmVehicle lista _indice
---   print rmedVehicle
+optionRemoveVehicle :: IO()
+optionRemoveVehicle = do 
+  putStrLn "\n\n\n### Remoção de veículo ###\n\n\n"
+  putStrLn "Índice do veículo: "
+  _vehicleId <- getLine
+  lista <- readFromJSON
+  let listaAtualizada = removeItem (read _vehicleId :: Int) lista
+  writeToJSON listaAtualizada
+  putStrLn "Lista atualizada:"
+  printVehicles listaAtualizada
 
+-- getItem :: Int -> [Vehicle] -> Maybe Vehicle
+-- getItem _ []                          = Nothing
+-- getItem x (y:ys) | x == (vehicleId) y = Just y
+--                  | otherwise = y      : getItem x ys
 
 -- optionUpdateVehicle :: IO()
 -- optionUpdateVehicle = do
 --   putStrLn "\n\n\n### Alteração de veículo ###"
---   putStrLn "Indice: "
---   _indice <- getLine
+--   putStrLn "\n\n\n### (Deixe em branco para não alterar) ###"
+--   putStrLn "vehicleId: "
+--   _vehicleId <- getLine
+--   lista <- readFromJSON
+--   let ve = getItem (read _vehicleId :: Int) lista
 --   putStrLn "Placa: "
---   _placa <- getLine
+--   _plate <- getLine
 --   putStrLn "Quilometragem: "
 --   _kms <- getLine
 --   putStrLn "Categoria: "
@@ -66,22 +80,34 @@ optionAddVehicle :: IO()
 optionAddVehicle = do
   putStrLn "\n\n\n### Cadastro de veículo ###"
   putStrLn "Placa: "
-  _placa <- getLine
+  _plate <- getLine
   putStrLn "Quilometragem: "
   _kms <- getLine
   putStrLn "Categoria: "
   _category <- getLine
+  putStrLn "Modelo: "
+  _model <- getLine
+  putStrLn "Marca: "
+  _brand <- getLine
+  putStrLn "Cor: "
+  _color <- getLine
+  putStrLn "Ano: "
+  _year <- getLine
   lista <- readFromJSON
-  let ve = Vehicle {indice = generateIndex lista, placa = _placa, kms = read _kms :: Int, category = _category}
+  let ve = Vehicle {vehicleId = generateVehicleId lista, plate = _plate, kms = read _kms :: Int, category = _category, model=_model, brand = _brand, color = _color, year = read _year :: Int}
   let list = addToList lista ve
-  print list
+
+  putStrLn "\nVeiculo adicionado: "
+  print ve
+  putStrLn "\n"
+  
   writeToJSON list
 
-generateIndex :: [Vehicle] -> Int
-generateIndex [] = 0
-generateIndex x = do 
+generateVehicleId :: [Vehicle] -> Int
+generateVehicleId [] = 0
+generateVehicleId x = do 
               let lastVeiculo = last x
-              (indice) lastVeiculo + 1
+              (vehicleId) lastVeiculo + 1
 
 addToList :: [Vehicle] -> Vehicle -> [Vehicle]
 addToList [] x = [x]
@@ -94,20 +120,34 @@ writeToJSON list = do
 readFromJSON :: IO [Vehicle]
 readFromJSON = do
   input <- B.readFile "db/vehicles.json"
-
   let vehicles = decode input :: Maybe [Vehicle]
-
   case vehicles of
     Nothing -> return []
     Just vehicles -> return vehicles
 
 
 printVehicles :: [Vehicle] -> IO ()
-printVehicles vehicles = putStrLn ("\n\n\n" ++ (listVehicle vehicles) ++ "\n\n")
+printVehicles vehicles = putStrLn ("\n\nID - Placa - Categoria - Marca - Modelo - Cor - Ano - Kms\n\n" ++ (listVehicle vehicles) ++ "\n")
 
 listVehicle :: [Vehicle] -> String
 listVehicle [] = ""
 listVehicle (x:xs) = toStringVehicle x ++ ['\n'] ++ listVehicle xs
 
 toStringVehicle :: Vehicle -> String
-toStringVehicle (Vehicle {indice = i, placa = p, kms = k, category = cat}) = show i ++ " - " ++ p ++ " - " ++ cat ++ " - " ++ show k ++ "km"
+toStringVehicle (Vehicle {
+  vehicleId = _vehicleId, 
+  plate = _plate, 
+  kms = _kms, 
+  category = _category, 
+  brand = _brand, 
+  model = _model,
+  color = _color, 
+  year = _year}) = 
+    show _vehicleId ++ " - " ++ 
+    _plate ++ " - " ++ 
+    _category ++ " - " ++ 
+    _brand ++ " - " ++ 
+    _model ++ " - " ++
+    _color ++ " - " ++ 
+    show _year ++ " - " ++ 
+    show _kms ++ "km" 
